158942cbc5a42c192da0a81d64977e447cb77a38
Brunno Warley <brunnowr.bw@gmail.com>
2023-04-17 18:59:33 +0200

    criado as funcionalidades de registro do modal de transações e controle de inputs usando a lib zod

--- src/pages/Transactions/components/SearchForm/index.tsx
+++ src/pages/Transactions/components/SearchForm/index.tsx
@@ -1,16 +1,42 @@
 import { MagnifyingGlass } from "phosphor-react";
 import { SearchFormContainer } from "./styles";
+import { useForm } from 'react-hook-form'
+import * as z from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
+
+const searchFormSchema = z.object({
+  query: z.string(),
+})
+
+type searchFormInputs = z.infer<typeof searchFormSchema>
 
 export function SearchForm() {
+  const {
+      register,
+      handleSubmit,
+      formState: { isSubmitting }
+       } = useForm<searchFormInputs>({
+    resolver: zodResolver(searchFormSchema)
+  })
+
+  async function handleSearchTransactions(data: searchFormInputs) {
+    await new Promise(resolve => setTimeout(resolve, 2000))
+    console.log(data)
+  }
+
   return (
-    <div>
-      <SearchFormContainer>
-        <input type="text" placeholder="Filtrar Transação" />
-        <button type="submit">
-          <MagnifyingGlass size={20}/>
+  
+      <SearchFormContainer onClick={handleSubmit(handleSearchTransactions)}>
+        <input type="text" 
+        placeholder="Filtrar Transação"
+        {...register('query')}
+         />
+        <button type="submit" disabled={ isSubmitting }>
+          <MagnifyingGlass size={20}
+          />
           Buscar
           </button>
       </SearchFormContainer>
-    </div>
+    
   );
 }
